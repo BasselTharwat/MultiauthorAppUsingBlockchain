2ac152b458073adf4eb8af27af6be775654caf70
BasselTharwat <bassel.tharwat01@gmail.com>
2024-05-01 18:56:02 +0300

    found a good graphing library

--- pages/stories/viewStory.js
+++ pages/stories/viewStory.js
@@ -10,12 +10,56 @@ import Files from '../../components/Files';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import factory from '../../ethereum/factory.js';
 
+import Graph from "react-graph-vis";
+//import "./styles.css";
+// need to import the vis network css in order to show tooltip
+//import "./network.css";
+
 
 const ViewStory = ({storyAddress}) => {
-    //const router = useRouter();
-    //const { asPath } = router; 
-    //const segments = asPath.split('/'); 
-    //const address = segments.pop(); 
+
+    const graph = {
+        nodes: [
+          { id: 1, label: "Node 1", title: "node 1 tootip text" },
+          { id: 2, label: "Node 2", title: "node 2 tootip text" },
+          { id: 3, label: "Node 3", title: "node 3 tootip text" },
+          { id: 4, label: "Node 4", title: "node 4 tootip text" },
+          { id: 5, label: "Node 5", title: "node 5 tootip text" },
+          { id: 6, label: "Node 6", title: "node 5 tootip text" }
+        ],
+        edges: [
+          { from: 1, to: 2 },
+          { from: 1, to: 3 },
+          { from: 2, to: 4 },
+          { from: 2, to: 5 },
+          { from: 6, to: 5 }
+        ]
+      };
+     
+      const options = {
+        layout: {
+            hierarchical: {
+                enabled: true,
+                parentCentralization: true,
+                sortMethod: 'directed',
+                shakeTowards: 'leaves'
+              }
+        },
+        edges: {
+          color: "#000000"
+        },
+        height: "500px"
+      };
+     
+      const events = {
+        select: function(event) {
+          const { nodes } = event;
+          if (nodes.length > 0) {
+            const nodeId = nodes[0]; // Get the ID of the first selected node
+            console.log("Selected node ID:", nodeId);
+          }
+        }
+      };
 
 
     const story = Story(storyAddress);
@@ -577,7 +621,11 @@ const ViewStory = ({storyAddress}) => {
                 </Modal>
                 
             </>
-                       
+            <Graph
+                graph={graph}
+                options={options}
+                events={events}
+            />           
         </Layout>
     );
 };
